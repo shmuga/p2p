16756749b0cd1efa69a3b97240057f570c3d120c
root <root@mta8.estismailer.com>
2015-10-31 01:05:19 +0100

    fixing bugs

---
+++ db.py
@@ -0,0 +1,24 @@
+from config import ERRORS,FILE,DB_HOST,DB_NAME,DB_PASSWORD,DB_USER
+import psycopg2
+
+def db_connect():
+    conn = psycopg2.connect("dbname='"+DB_NAME+"' user='"+DB_USER+"' host='"+DB_HOST+"' password='"+DB_PASSWORD+"'")
+    cur = conn.cursor()
+    return conn,cur
+
+def db_close(conn, curr):
+    curr.close()
+    conn.close()
+
+def insert_db(conn, cur, queueId, sendId, errorKey): 	
+    try:
+        cur.execute('SELECT * FROM exim WHERE queue_id=%s', (queueId,))
+        result = cur.fetchone()
+        if result == None:
+            cur.execute('INSERT INTO exim(queue_id,send_id,error_code,state) VALUES(%s,%s,%s,%s)', (queueId,sendId,errorKey,501,))
+            conn.commit()
+        else:
+            cur.execute('UPDATE exim SET error_code=%s,state=501 WHERE queue_id=%s', (errorKey,queueId))
+            conn.commit()
+    except Exception, e:
+        print e
---
+++ parser.py
@@ -0,0 +1,60 @@
+import time
+import re
+import db
+
+from datetime import datetime
+from config import ERRORS,FILE
+from os import rename,remove,path
+import collections
+errors = collections.OrderedDict()
+ERRORS = ERRORS.split('\n')
+for k in ERRORS:
+    if k:
+        err = k.split('=')
+        errors[err[1]] = err[0]
+
+def parse_file_infite():
+    filename = FILE
+    while True:
+        timeBefore = time.time()
+        newName = filename + 'stat' + str(int(time.time()))
+        if path.isfile(filename):
+            conn,curr = db.db_connect()
+            rename(filename, newName)
+            mainlog = open(newName, 'a+')
+	    mainlog.write('\nFrom MAILER-DAEMON');
+	    mainlog.close()
+	    mainlog = open(newName, 'a+')		
+            process_file(mainlog, conn, curr)
+            
+            db.db_close(conn, curr)
+        if time.time() - timeBefore < 60:
+            time.sleep(60)
+
+def process_file(mainlog, conn, curr):
+    letter = []
+    for line in mainlog:
+        if line.startswith('From MAILER-DAEMON') or line == '':
+            # after tihs we get letter error
+            errorKey = process_letter(''.join(letter))
+            if not errorKey:
+                errorKey = 666
+            idGroup = find_id(''.join(letter))
+            if idGroup:
+                queueId, sendId = idGroup.groups()
+                # inserting to db
+                db.insert_db(conn, curr, queueId, sendId, errorKey)
+            letter = []
+        letter.append(line)
+
+def process_letter(letter):
+    letter = ''.join(letter)
+    returnKey = None
+    for value, key in errors.iteritems():
+        if letter.find(value) != -1:
+            returnKey = key
+            break
+    return returnKey
+
+def find_id(letter):
+    return re.search('Message-ID: <(\d+)\.(\d+)@', letter)
